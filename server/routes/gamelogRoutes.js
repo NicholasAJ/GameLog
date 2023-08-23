bf93689643d66de47d412d5c619b182ff64907f3
Nicholas Juliana <naj1243@gmail.com>
2023-08-22 20:33:30 -0400

    gamelog controller

--- server/routes/gamelogRoutes.js
+++ server/routes/gamelogRoutes.js
@@ -1,9 +1,9 @@
 const gamelogController = require("../controllers/gamelogController");
 
 module.exports = (app) => {
-  app.post('/api/create/log', gamelogController.createLog);
-  app.put('/api/update/log', gamelogController.updateLog);
-  app.get('/api/read', gamelogController.getOneLog);
-  app.get('/api/read/all', gamelogController.getAllLogs);
-  app.delete('/api/delete/log', gamelogController.deleteLog);
+  app.post('/api/create/game', gamelogController.createGame);
+  app.get('/api/read/game', gamelogController.getOneGame);
+  app.get('/api/read/game/all', gamelogController.getAllGames);
+  app.put('/api/update/game', gamelogController.updateGame);
+  app.delete('/api/delete/game', gamelogController.deleteGame);
 }
